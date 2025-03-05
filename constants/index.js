73c6d974bed115947fe2e7e626e1e7f6012a7fe5
Amir Husain <amir.developer786@outlook.com>
2025-03-05 11:04:54 +0530

    constant folder approved

---
+++ constants/index.js
@@ -0,0 +1,25 @@
+export const apiKey = "Place your api key here";
+
+export const moviesData = [
+    {
+        id: 2,
+        title: "Ant-man and the Wasp: Quantumania",
+
+        backgroundPoster: "https://image.tmdb.org/t/p/w500/9Hk9qdCyce04VXNQudXAK1d138E.jpg",
+        image: "https://image.tmdb.org/t/p/w500/qnqGbB22YJ7dSs4o6M7exTpNxPz.jpg",
+        description: "Super-hero partners Scott Land and Hope van Dyne",
+        releaseDate: "1991-01-18",
+        releaseYear: "2020",
+        runTime: "170"
+    }
+]
+
+export const personData = {
+    name: "Keanu Reeves",
+    birthPlace: "Beirut, Lebanon",
+    birthday: "1964-09-02",
+    gender: "Male",
+    knownFor: "Acting",
+    popularity: "74.86",
+    biography: "Keanu Charles Reeves (September 2, 1964) is a Canadian[c] actorthe Canadian television series Hangin. In (1984), before making his feature film debut in Youngblood (1986)."
+}
